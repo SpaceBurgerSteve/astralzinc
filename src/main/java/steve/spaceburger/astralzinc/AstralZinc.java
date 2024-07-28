2142aa3e08fb1112957f73b232a11f18161465fd
SpaceBurgerSteve <space.burger.steve@gmail.com>
2024-07-28 09:36:53 -0700

    Added ModBlocks.java

--- src/main/java/steve/spaceburger/astralzinc/AstralZinc.java
+++ src/main/java/steve/spaceburger/astralzinc/AstralZinc.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+import steve.spaceburger.astralzinc.block.ModBlocks;
 
 @Mod(AstralZinc.MOD_ID)
 public class AstralZinc
@@ -25,6 +26,8 @@ public class AstralZinc
     public AstralZinc()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
