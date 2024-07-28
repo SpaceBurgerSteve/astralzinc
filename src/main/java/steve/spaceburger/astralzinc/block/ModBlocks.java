c62f59445584efd3ef34c9ab285747ca077c03c7
SpaceBurgerSteve <space.burger.steve@gmail.com>
2024-07-28 09:38:08 -0700

    Added ModBlocks.java

---
+++ src/main/java/steve/spaceburger/astralzinc/block/ModBlocks.java
@@ -0,0 +1,39 @@
+package steve.spaceburger.astralzinc.block;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import steve.spaceburger.astralzinc.AstralZinc;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, AstralZinc.MOD_ID);
+
+    public static final RegistryObject<Block> MARS_ZINC_ORE= registerBlock("mars_zinc_ore",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.GOLD_ORE)));
+
+    public static final RegistryObject<Block> GLACIO_ZINC_ORE= registerBlock("glacio_zinc_ore",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.GOLD_ORE)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block){
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+}
